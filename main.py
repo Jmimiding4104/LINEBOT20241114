4a9adfb9f09e506aa7efa876144a04dee75e977c
test <test@gmail.com>
2024-11-15 15:02:46 +0800

    v3

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from linebot.v3.messaging import (
     PostbackAction
 )
 from linebot.v3.webhooks import MessageEvent, TextMessageContent, PostbackEvent
+import re
 
 app = Flask(__name__)
 
@@ -26,6 +27,51 @@ configuration = Configuration(
     access_token='mh/r45bGW1j8Nvk2G/X8/1j+jf/0H60retZSLmLZ2bCJtwuMRB308Vnk5/LHQ4Yk2uGR/rkCQYoUvnqOl20BHaR8LmQTCWy4kldRqfUn5rBqRIQxUA171It7o+mRHPJHfU7H/v8H9ZZRQ0b/pxEmuQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('84d36b609616d351c7c3cba259f0b769')
 
+def send_operation_options(line_bot_api, reply_token):
+    buttons_template = ButtonsTemplate(
+        title="請問你要進行什麼操作？",
+        text="請點擊以下選項",
+        actions=[
+            PostbackAction(label="開始集點", data="start"),
+            PostbackAction(label="不需要操作", data="logout")
+        ]
+    )
+    
+    template_message = TemplateMessage(
+        alt_text="請問你要進行什麼操作？",
+        template=buttons_template
+    )
+    line_bot_api.reply_message_with_http_info(
+        ReplyMessageRequest(
+            reply_token=reply_token,
+            messages=[template_message]
+        )
+    )
+    
+def send_other_operation_options(line_bot_api, reply_token):
+    buttons_template = ButtonsTemplate(
+        title="請問你還需要處理其他項目嗎？",
+        text="請點擊以下選項",
+        actions=[
+            PostbackAction(label="生理監測", data="monitor"),
+            PostbackAction(label="AI衛教", data="educate"),
+            PostbackAction(label="運動", data="exercise"),
+            PostbackAction(label="登出", data="logout")
+        ]
+    )
+    
+    template_message = TemplateMessage(
+        alt_text="請問你還需要處理其他項目嗎？",
+        template=buttons_template
+    )
+    line_bot_api.reply_message_with_http_info(
+        ReplyMessageRequest(
+            reply_token=reply_token,
+            messages=[template_message]
+        )
+    )
+    
+    
 
 @app.route("/", methods=['POST'])
 def linebot():
@@ -112,8 +158,8 @@ def handle_message(event):
                     messages=[template_message]
                 )
             )
-
-
+        elif re.match(r'^[A-Za-z]\d{9}$', event.message.text) or user_info["step"] == 4:
+            send_operation_options(line_bot_api, tk)
 @handler.add(PostbackEvent)
 def handle_postback(event):
     global user_info
@@ -128,7 +174,7 @@ def handle_postback(event):
             line_bot_api.reply_message_with_http_info(
                 ReplyMessageRequest(
                     reply_token=tk,
-                    messages=[TextMessage(text="感謝註冊！")]
+                    messages=[TextMessage(text="請輸入會員身分證字號")]
                 )
             )
         elif data == "incorrect":
@@ -145,7 +191,48 @@ def handle_postback(event):
                     messages=[TextMessage(text="請重新輸入姓名")]
                 )
             )
-
+        elif data == "start":
+            buttons_template = ButtonsTemplate(
+            title="請問你要處理哪個項目？",
+            text="請點擊以下選項",
+            actions=[
+                PostbackAction(label="生理監測", data="monitor"),
+                PostbackAction(label="AI衛教", data="educate"),
+                PostbackAction(label="運動", data="exercise"),
+                PostbackAction(label="登出", data="logout")
+                ]
+            )
+    
+            template_message = TemplateMessage(
+            alt_text="請問你要進行什麼集點？",
+            template=buttons_template
+            )
+            line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=tk,
+                messages=[template_message]
+                )
+            )
+        elif data == "logout":
+            user_info = {  # 重設 user_info
+                    "name": "",
+                    "idNumber": "",
+                    "tel": "",
+                    "step": 0
+                }
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=tk,
+                    messages=[TextMessage(text="登出成功")]
+                )
+            )
+        elif data == "monitor":
+            send_other_operation_options(line_bot_api, tk)
+        elif data == "educate":
+            send_other_operation_options(line_bot_api, tk)
+        elif data == "exercise":
+            send_other_operation_options(line_bot_api, tk)
+        
 if __name__ == "__main__":
     app.run()
 
